5b215789bff29ddaad152061e8a4a2e98fb5e15e
Jerry <jerryhxie@gmail.com>
2026-02-17 09:37:56 -0700

    Forgot to warm up Pathplanner oops

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.commands.PathfindingCommand;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -30,6 +32,9 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    // Warms up Pathplanner for on-the-fly path generation
+    PathfindingCommand.warmupCommand().schedule();
   }
 
   /**
